0676f175957b834297b74a9fb4853418339fb6b1
6xenny <6xeshi@gmail.com>
2025-05-31 23:44:07 -0500

    Add files via upload

---
+++ main code/auto update hitsounds.lua
@@ -0,0 +1,11 @@
+local Sounds = {
+	["Rust.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/Rust.wav",
+	["skeet.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/skeet.wav",
+	["neverlose.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/neverlose.wav",
+	["fatality.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/fatality.wav",
+	["phonk.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/phonk.wav",
+	["bonk.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/bonk.wav",
+	["pluh.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/pluh.wav",
+	["sparkle.wav"] = "https://github.com/6xenny/flatline/raw/refs/heads/main/hit%20sounds/sparkle.wav",
+}
+return Sounds
